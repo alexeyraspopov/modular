acc1f03dbae7843df86ad0fa6fbc749316db9621
Avenger7x <avenger7x13@gmail.com>
2013-11-24 17:50:35 +0200

    clean up

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,15 @@ module.exports = function(grunt){
 	var scripts = { src: 'modular.js' };
 
 	grunt.initConfig({
+		bump: {
+			options: {
+				files: ['package.json', 'bower.json'],
+				commitFiles: ['-a'],
+				commitMessage: 'release %VERSION%',
+				tagMessage: 'version %VERSION%',
+				pushTo: 'origin'
+			}
+		},
 		complexity: {
 			target: scripts
 		},
@@ -19,6 +28,7 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-complexity');
+	grunt.loadNpmTasks('grunt-bump');
 
 	grunt.registerTask('default', ['jshint', 'complexity', 'uglify']);
 };
